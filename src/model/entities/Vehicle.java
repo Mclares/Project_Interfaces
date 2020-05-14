181778f562733b919afe044e88cf27fa1dec421f
Marcelo Clares <msc.09.wylde@gmail.com>
2020-05-14 18:07:20 -0300

    Projeto com Interfaces

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,22 @@
+package model.entities;
+
+public class Vehicle {
+	
+	private String Model;
+
+	public Vehicle() {
+	}
+	
+	public Vehicle(String model) {
+		Model = model;
+	}
+
+	public String getModel() {
+		return Model;
+	}
+
+	public void setModel(String model) {
+		Model = model;
+	}
+
+}
